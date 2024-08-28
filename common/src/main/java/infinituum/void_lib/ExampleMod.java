eebaaa5740635625ab74cbba05f936fab0882a4c
Infinituum17 <40234342+Infinituum17@users.noreply.github.com>
2024-08-28 15:09:17 +0200

    🎉 First commit!

---
+++ common/src/main/java/infinituum/void_lib/ExampleMod.java
@@ -0,0 +1,9 @@
+package infinituum.void_lib;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "void_lib";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
