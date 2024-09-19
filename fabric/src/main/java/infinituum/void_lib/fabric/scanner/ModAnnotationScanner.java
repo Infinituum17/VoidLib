05b6bd9ea97336f07dda0e5c649993ae80e5d2f7
Infinituum <40234342+Infinituum17@users.noreply.github.com>
2024-09-19 02:33:56 +0200

    🔧 Hotfix for generated mods not detected correctly

--- fabric/src/main/java/infinituum/void_lib/fabric/scanner/ModAnnotationScanner.java
+++ fabric/src/main/java/infinituum/void_lib/fabric/scanner/ModAnnotationScanner.java
@@ -94,7 +94,8 @@ public final class ModAnnotationScanner {
                         .stream()
                         .anyMatch(dependency -> dependencyId.equals(dependency.getModId()))
                         || dependencyId.equals(modFile.getModId())
-                        || (FabricLoader.getInstance().isDevelopmentEnvironment() && modFile.getModId().isEmpty()))
+                        || (FabricLoader.getInstance().isDevelopmentEnvironment() && modFile.getModId().isEmpty())
+                        || modFile.getModId().startsWith("generated_"))
                 .filter(modFile -> modFile
                         .getAnnotatedClasses()
                         .stream()
