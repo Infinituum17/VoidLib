6a99cc8e6bb90273ffd68e8c0c630d61e7a767c9
Infinituum <40234342+Infinituum17@users.noreply.github.com>
2024-09-20 16:06:05 +0200

    🔧 First 1.21 release! Replaced Forge with Neoforge

---
+++ neoforge/src/main/java/infinituum/void_lib/neoforge/VoidLibNeoForge.java
@@ -0,0 +1,11 @@
+package infinituum.void_lib.neoforge;
+
+import infinituum.void_lib.VoidLib;
+import net.neoforged.fml.common.Mod;
+
+@Mod(VoidLib.MOD_ID)
+public final class VoidLibNeoForge {
+    public VoidLibNeoForge() {
+        VoidLib.init();
+    }
+}
